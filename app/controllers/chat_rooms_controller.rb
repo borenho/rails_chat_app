553dfc6370aec8e96dd01cbab47dbee21e32ac47
Ruto Collins <borecollins@gmail.com>
2019-10-08 03:29:36 +0300

    Create a new chat room

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,26 @@
 class ChatRoomsController < ApplicationController
   def index
+    @chat_rooms = ChatRoom.all
+  end
+
+  def new
+    @chat_room = ChatRoom.new
+  end
+
+  def create
+    @chat_room = ChatRoom.new permitted_params
+
+    if @chat_room.save
+      flash[:success] = "ChatRoom #{@chat_room.topic} was created successfully"
+      redirect_to chat_rooms_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def permitted_params
+    params.require(:chat_room).permit(:topic)
   end
 end
