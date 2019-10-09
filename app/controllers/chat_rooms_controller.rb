57b5c17a6d3318c5f340f15008c46c89cd8abf59
Ruto Collins <borecollins@gmail.com>
2019-10-09 10:24:15 +0300

    Style the show each and all chat rooms

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -3,6 +3,12 @@ class ChatRoomsController < ApplicationController
     @chat_rooms = ChatRoom.all
   end
 
+  def show
+    load_entities
+    @message = Message.new chat_room: @chat_room
+    @messages = @chat_room.messages.includes(:user)
+  end
+
   def new
     @chat_room = ChatRoom.new
   end
@@ -18,9 +24,28 @@ class ChatRoomsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    load_entities
+    if @chat_room.update_attributes(permitted_params)
+      flash[:success] = "ChatRoom #{@chat_room.topic} was successfully updated"
+      redirect_to chat_rooms_path
+    else
+      render :new
+    end
+  end
+
   protected
 
   def permitted_params
     params.require(:chat_room).permit(:topic)
+  end
+
+  def load_entities
+    @chat_rooms =  ChatRoom.all
+    @chat_room = ChatRoom.find(params[:id]) if params[:id]
   end
 end
